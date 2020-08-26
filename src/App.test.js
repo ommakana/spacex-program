8df73f5874432aeed053f3c7536889c15a2685fd
Makana, Om <OMakana@mast.com>
2020-08-26 19:31:03 +0530

    started unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {Provider} from 'react-redux';
+import store from "./reducer/index";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render( <Provider store={store}>
+    <App name="SpaceX Launch Programs" />
+  </Provider>);
+  const linkElement = getByText(/SpaceX Launch Programs/i);
   expect(linkElement).toBeInTheDocument();
 });
