edd50bddc7fbfc9df4d6cd13569ada54882631fd
Makana, Om <OMakana@mast.com>
2020-08-26 13:23:58 +0530

    url params update on filter click

--- src/component/home.js
+++ src/component/home.js
@@ -19,54 +19,38 @@ class Home extends React.Component {
       })
   }
 
-  handleYearClick = (event) => {
+  handleFilter = (event) => {
 
     let url = new URL(window.location.href);
     let urlParams = new URLSearchParams(url.search);
+    let filterByValue = event.target.name;
+    if (urlParams.has(filterByValue)) {
+      let newUrl = window.location.href;
+      let replaceValue = window.location.search.split(/([?,&])/).find(item =>
+        item.includes(filterByValue)
+      );
+      window.history.pushState(null, null, newUrl.replace(replaceValue, `${filterByValue}=${event.target.value}`));
+    }
+    else {
+      if (this.state.firstParam) {
+        window.history.pushState(null, null, `${window.location.href}?${filterByValue}=${event.target.value}`);
+        this.setState({ firstParam: false });
+      }
+      else {
+        window.history.pushState(null, null, `${window.location.href}&${filterByValue}=${event.target.value}`);
+      }
+    }
 
-    // console.log(urlParams.has("launch_year"));
-    // if (urlParams.has("launch_year")) {
-    //   let newUrl = window.location.href;
-    //   let launchYear = window.location.search.split("? | &").find(item =>
-    //     item.includes("launch_year")
-    //   );
-    //   console.log(launchYear);
-    //   window.history.pushState(null, null, newUrl.replace(launchYear, `launch_year=${event.target.value}`));
-    // }
-    // else {
-    //   if(this.state.firstParam) {
-    //     window.history.pushState(null, null, `${window.location.href}?launch_year=${event.target.value}`);
-    //     this.setState({firstParam: false});
-    //   }
-    //   else{
-    //     window.history.pushState(null, null, `${window.location.href}&launch_year=${event.target.value}`);
-    //   }
-    // }
-    window.history.pushState(null, null, `${window.location.href}?launch_year=${event.target.value}`);
-  }
-
-  handleLaunchSuccess = event => {
-    // let urlParams = new URLSearchParams(window.location.search);
-    // if (urlParams.has("launch_success")) {
-    //   let newUrl = window.location.href;
-    //   let launchSuccess = window.location.search.split("?").find(item =>
-    //     item.includes("launch_success")
-    //   )
-    //   window.history.pushState(null, null, newUrl.replace(launchSuccess, `launch_success=${event.target.value}`));
-    // }
-    // else {
-    //   window.history.pushState(null, null, `${window.location.href}?launch_success=${event.target.value}`);
-    // }
   }
 
   render() {
     return (<React.Fragment>
       <h1>{this.props.name}</h1>
-      <button onClick={this.handleYearClick} value="2014">2014</button>
-      <button onClick={this.handleYearClick} value="2015">2015</button>
+      <button onClick={this.handleFilter} name="launch_year" value="2014">2014</button>
+      <button onClick={this.handleFilter} name="launch_year" value="2015">2015</button>
       <h3>Launch success</h3>
-      <button onClick={this.handleLaunchSuccess} value="true">True</button>
-      <button onClick={this.handleLaunchSuccess} value="false">False</button>
+      <button onClick={this.handleFilter} name="launch_success" value="true">True</button>
+      <button onClick={this.handleFilter} name="launch_success" value="false">False</button>
       <div className="container">
         {!this.props.loading ? this.props.data.map((item, index) =>
           <p key={index}>{item.mission_name}</p>
