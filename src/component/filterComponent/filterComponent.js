47e1fc44f1d7f8a33cedd04d30f129b558a7af0a
Om Makana <ommakana@Oms-MacBook-Pro.local>
2020-09-15 19:33:18 +0530

    Using event delegation & removing multiple onClicks from button elements

--- src/component/filterComponent/filterComponent.js
+++ src/component/filterComponent/filterComponent.js
@@ -5,7 +5,7 @@ import { LAUNCH_YEAR, LAND_SUCCESS, LAUNCH_SUCCESS } from "../../constants";
 
 import './filterComponent.css';
 
-export const FilterComponent = memo(({ handleFilter }) => {
+export const FilterComponent = memo(() => {
 
     return (<>
         <h2 className="filter-header-text">Filters</h2>
@@ -13,7 +13,7 @@ export const FilterComponent = memo(({ handleFilter }) => {
         <p className="filter-header">Launch Year</p>
 
         <div className="filter-button-container">
-            {launchYear.map((item, index) => <button onClick={handleFilter}
+            {launchYear.map((item, index) => <button
                 className="filter-button" value={item}
                 name={LAUNCH_YEAR} key={index}>{item}
             </button>)}
@@ -22,22 +22,22 @@ export const FilterComponent = memo(({ handleFilter }) => {
         <p className="filter-header">Successful Launch</p>
 
         <div className="filter-button-container">
-            {booleanButton(LAUNCH_SUCCESS, handleFilter)}
+            {booleanButton(LAUNCH_SUCCESS)}
         </div>
 
         <p className="filter-header">Successful Landing</p>
 
         <div className="filter-button-container">
-            {booleanButton(LAND_SUCCESS, handleFilter)}
+            {booleanButton(LAND_SUCCESS)}
         </div>
     </>
     )
 });
 
-function booleanButton(name, handleFilter) {
+function booleanButton (name) {
     return (<>
-        <button className="filter-button" name={name} onClick={handleFilter} value="true">True</button>
-        <button className="filter-button" name={name} onClick={handleFilter} value="false">False</button>
+        <button className="filter-button" name={name} value="true">True</button>
+        <button className="filter-button" name={name} value="false">False</button>
     </>
     )
 }
--- src/component/home/home.js
+++ src/component/home/home.js
@@ -11,32 +11,31 @@ import { FilterComponent } from "../filterComponent/filterComponent";
 
 class Home extends React.PureComponent {
 
-  componentDidMount() {
-    this.props.fetchData();
-    axios.get(`${apiURL}`)
-      .then(res => {
-        this.props.loadData(res.data);
-      })
-  }
+componentDidMount() {
+  this.props.fetchData();
+  axios.get(`${apiURL}`)
+    .then(res => {
+      this.props.loadData(res.data);
+    })
+}
 
-  handleFilter = (event) => {
-
+handleFilter = (event) => {
+  if (event.target.name && event.target.value) {
     let url = new URL(window.location.href);
     let urlParams = new URLSearchParams(url.search);
     let filterByValue = event.target.name;
+  
     if (urlParams.has(filterByValue)) {
       let newUrl = window.location.href;
       let replaceValue = window.location.search.split(/([?,&])/).find(item =>
         item.includes(filterByValue)
       );
       window.history.pushState(null, null, newUrl.replace(replaceValue, `${filterByValue}=${event.target.value}`));
-    }
-    else {
+    } else {
       if (this.props.isFirstQueryParam) {
         window.history.pushState(null, null, `${window.location.href}?${filterByValue}=${event.target.value}`);
         this.props.firstQueryParam(false);
-      }
-      else {
+      } else {
         window.history.pushState(null, null, `${window.location.href}&${filterByValue}=${event.target.value}`);
       }
     }
@@ -46,28 +45,27 @@ class Home extends React.PureComponent {
     axios.get(generateApiUrl())
       .then(res => {
         this.props.loadData(res.data);
-      })
-
-  }
-
-  clearAllFilters = () => {
-
-    this.props.fetchData();
-    axios.get(`${apiURL}`)
-      .then(res => {
-        this.props.loadData(res.data);
-        this.props.firstQueryParam(true);
-        window.history.pushState(null, null, `${window.location.pathname}`);
       });
-  
-   }
+  }
+}
+
+clearAllFilters = () => {
+  this.props.fetchData();
+
+  axios.get(`${apiURL}`)
+    .then(res => {
+      this.props.loadData(res.data);
+      this.props.firstQueryParam(true);
+      window.history.pushState(null, null, `${window.location.pathname}`);
+    });
+  }
 
   render() {
     return (<div className="inner_container">
 
-      <div className="filter_wrapper">
+      <div className="filter_wrapper" onClick={this.handleFilter}>
 
-        <FilterComponent handleFilter={this.handleFilter} />
+        <FilterComponent />
         <br/>
         <button className="filter-button" onClick={this.clearAllFilters}>Clear all</button>
       </div>
@@ -78,7 +76,6 @@ class Home extends React.PureComponent {
         ) : <h2>No results found . . .</h2> : <h2>Loading. . . . .</h2>
         }
       </div>
-
     </div>
     )
   }
